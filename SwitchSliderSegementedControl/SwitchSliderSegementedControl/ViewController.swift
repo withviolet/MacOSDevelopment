2be659b71ce6252b5a630029e7369b0ac4d906ca
chenxiaohan <chenxiaohan@tenorshare.com>
2021-09-06 20:16:15 +0800

    [SwitchSliderSegementedControl]:练习segement控件和滑动条控件的使用，IB实现

--- SwitchSliderSegementedControl/SwitchSliderSegementedControl/ViewController.swift
+++ SwitchSliderSegementedControl/SwitchSliderSegementedControl/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var leftSwitch: UISwitch!
     @IBOutlet weak var rightSwitch: UISwitch!
+    @IBOutlet weak var segementSwitch: UISegmentedControl!
+    @IBOutlet weak var sliderValueLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,10 +21,37 @@ class ViewController: UIViewController {
 
     @IBAction func switchValueChanged(_ sender: Any) {
         let witchSwitch = sender as! UISwitch
+        //isOn是要变成的状态
         let setting = witchSwitch.isOn
         self.leftSwitch.setOn(setting, animated: true)
         self.rightSwitch.setOn(setting, animated: true)
     }
+    
+    @IBAction func touchDown(_ sender: Any) {
+        let segementedControl = sender as! UISegmentedControl
+        print("choosed : ",segementedControl.selectedSegmentIndex)
+        
+        if(self.leftSwitch.isHidden == true) {
+            self.rightSwitch.isHidden = false
+            self.leftSwitch.isHidden = false
+        }else{
+            self.rightSwitch.isHidden = true
+            self.leftSwitch.isHidden = true
+        }
+    }
+    
+    @IBAction func sliderValueChange(_ sender: Any) {
+//        if(sender != nil) {
+//
+//        }else{
+//            let slider =
+//        }
+        let slider = sender as! UISlider
+        let progressAsInt = Int(slider.value)
+        let newText = String(progressAsInt)
+        self.sliderValueLabel.text = newText
+    }
+    
     
 }
 
