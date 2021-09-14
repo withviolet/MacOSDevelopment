e2a42c789d772f161880d01005d7e5cf818b4c02
chenxiaohan <chenxiaohan@tenorshare.com>
2021-09-14 15:22:06 +0800

    [SearchbarTable]:未显示图片

---
+++ 第六章表视图/SearchbarTable/SearchbarTable/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  SearchbarTable
+//
+//  Created by Chen Xiaohan on 2021/9/14.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- 第六章表视图/SearchbarTable/SearchbarTable/ViewController.swift
+++ 第六章表视图/SearchbarTable/SearchbarTable/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController,UISearchBarDelegate,UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
+    
+    var searchController: UISearchController!
+    var listTeams: NSArray!
+    var listFilterTeams: NSArray!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let plistPath = Bundle.main.path(forResource: "team", ofType: "plist")
+        self.listTeams = NSArray(contentsOfFile: plistPath!)
+        
+        self.searchController = UISearchController(searchResultsController: nil)
+        self.searchController.searchResultsUpdater = self
+        self.searchController.obscuresBackgroundDuringPresentation = false
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier", for: indexPath) as! CustomTableViewCell
+        return cell
+    }
+    
 }
 
