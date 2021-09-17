fdd0b0de36a794d6ee68d47573abbf054870d3e4
chenxiaohan <chenxiaohan@tenorshare.com>
2021-09-17 11:38:24 +0800

    [StaticTable]:静态表视图布局练习

---
+++ 第7章界面布局/StaticTable/StaticTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StaticTable
+//
+//  Created by Chen Xiaohan on 2021/9/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
